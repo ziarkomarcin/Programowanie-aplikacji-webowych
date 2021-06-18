864f67a63628d682d8c1d7dbef2c73a7fd8ba49f
Marcin Ziarko <61658876+ziarkomarcin@users.noreply.github.com>
2021-06-18 18:33:59 +0200

    the smallest update ever

--- pogodynka/src/index.ts
+++ pogodynka/src/index.ts
@@ -1,4 +1,7 @@
 import { App } from './app';
 import './main.scss';
 
+let addToCityList = document.getElementById("button-to-add");
+let showCities = document.getElementById("show-cities-div");
+
 const app = new App();
