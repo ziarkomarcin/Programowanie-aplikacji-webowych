129b6cf205a4341add408e72a06025ffcad70dc4
Marcin Ziarko <61658876+ziarkomarcin@users.noreply.github.com>
2021-06-23 15:15:43 +0200

    helper -> colors

---
+++ noteKeep + fireBase/src/my_Helper.js
@@ -0,0 +1,10 @@
+"use strict";
+exports.__esModule = true;
+exports.Colors = void 0;
+var Colors;
+(function (Colors) {
+    Colors[Colors["lightred"] = 0] = "lightred";
+    Colors[Colors["lightgreen"] = 1] = "lightgreen";
+    Colors[Colors["lightblue"] = 2] = "lightblue";
+    Colors[Colors["cream"] = 3] = "cream";
+})(Colors = exports.Colors || (exports.Colors = {}));
---
+++ noteKeep + fireBase/src/my_Helper.ts
@@ -0,0 +1,6 @@
+export enum Colors {
+    lightred = 0,
+    lightgreen = 1,
+    lightblue = 2,
+    cream = 3
+}
